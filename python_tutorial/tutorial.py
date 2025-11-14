c3f1a4c70966db95508ae783d75b0d9d0e92f8d0
Elias Bandoh <ebandoh27@gmail.com>
2025-11-14 11:25:41 +0000

    Recapping Python Fundametals

--- python_tutorial/tutorial.py
+++ python_tutorial/tutorial.py
@@ -1,2 +1,29 @@
+#Task 1: Print "Hello World" if 5 is greater than 2
+'''
 if 5 > 2:
-    print("Hello World")
+    print("Hello World")'''
+#Task 2: Assign values to variables and print them
+'''
+x = 5
+y = 'Hello World'
+print(x)
+print(y)'''
+
+#Mixing Text and Numbers 
+
+print('I am', 35, 'years old')
+
+#Many Values to Multiple Variables
+
+x, y, z = 'Orange', 'Banana', 'Cherry'
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+
+fruits = {'Apple', 'Banana', 'Cherry'}
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
